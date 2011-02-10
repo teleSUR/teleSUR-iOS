39bfbd45a0cfd7e339c2c356214d9a145f75fa3f
David Regla <dreglad@gmail.com>
2011-02-10 17:24:01 -0600

    cambiado mensaje de copyright

--- iPad/AppDelegate_iPad.m
+++ iPad/AppDelegate_iPad.m
@@ -3,7 +3,7 @@
 //  teleSUR-iOS
 //
 //  Created by David Regla on 2/10/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 teleSUR. All rights reserved.
 //
 
 #import "AppDelegate_iPad.h"
