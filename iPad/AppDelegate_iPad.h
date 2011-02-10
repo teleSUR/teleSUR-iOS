4fb7cf4ac69e11631211328f85054c1ea4dcbb6c
David Regla <dreglad@gmail.com>
2011-02-10 17:22:23 -0600

    Carga inicial con proyecto nuevo en Xcode, Window-Bassed, universa, Core Data

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPad.h
+//  teleSUR-iOS
+//
+//  Created by David Regla on 2/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPad : AppDelegate_Shared {
+}
+
+
+@end
+
---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPhone.h
+//  teleSUR-iOS
+//
+//  Created by David Regla on 2/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPhone : AppDelegate_Shared {
+}
+
+
+@end
+
