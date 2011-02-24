ba6e31dab44218897ff9f200ede042b448eb47fe
David Regla <dreglad@gmail.com>
2011-02-24 05:20:40 -0600

    se agregó una clase nueva para implementar controlador de vista de listado de clips

---
+++ TSCLipsListadoTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TSCLipsListadoTableViewController.h
+//  teleSUR-iOS
+//
+//  Created by David Regla on 2/24/11.
+//  Copyright 2011 teleSUR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TSCLipsListadoTableViewController : UITableViewController {
+    NSArray *clips;
+}
+
+@end
