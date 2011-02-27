1b2368c46ec45c3a1e725126479b9d22bf882ef5
David Regla <dreglad@gmail.com>
2011-02-26 20:49:12 -0600

    agregado nuevo View Controller para detalles de clip

---
+++ teleSUR/TSClipDetallesViewController.h
@@ -0,0 +1,16 @@
+//
+//  TSClipDetallesViewController.h
+//  teleSUR
+//
+//  Created by David Regla on 2/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TSClipDetallesViewController : UIViewController {
+    
+}
+
+@end
