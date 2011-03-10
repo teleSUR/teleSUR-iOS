731c68f2e8394ab840e2a0c23bc791c8a9428acb
David Regla <dreglad@gmail.com>
2011-03-10 12:35:28 -0600

    Agregada nueva clase que representa vista de búsqueda

---
+++ teleSUR/TSClipBusquedaViewController.h
@@ -0,0 +1,16 @@
+//
+//  TSClipBusquedaViewController.h
+//  teleSUR
+//
+//  Created by David Regla on 3/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TSClipBusquedaViewController : UIViewController {
+    
+}
+
+@end
