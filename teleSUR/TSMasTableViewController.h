0dd2f33f617f80d105dfcaaeaf23007ce0f2c981
Hector Zarate <hecktorzr@gmail.com>
2011-03-30 15:53:01 -0600

    Vista Más

---
+++ teleSUR/TSMasTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TSMasTableViewController.h
+//  teleSUR
+//
+//  Created by Hector Zarate on 3/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TSMasTableViewController : UITableViewController {
+    
+}
+
+@end
