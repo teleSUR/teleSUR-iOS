4321f8bdfcec91a5fc455ce11a3a73b7aac512db
Hector Zarate <hecktorzr@gmail.com>
2011-06-17 17:40:37 -0500

    Imagen-Boton en Anotaciones de Mapa

---
+++ teleSUR/AsynchronousButtonView.h
@@ -0,0 +1,16 @@
+//
+//  AsynchronousButtonView.h
+//  teleSUR
+//
+//  Created by Hector Zarate Rea on 6/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AsynchronousButtonView : UIButton {
+    
+}
+
+@end
---
+++ teleSUR/AsynchronousButtonView.m
@@ -0,0 +1,14 @@
+//
+//  AsynchronousButtonView.m
+//  teleSUR
+//
+//  Created by Hector Zarate Rea on 6/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AsynchronousButtonView.h"
+
+
+@implementation AsynchronousButtonView
+
+@end
